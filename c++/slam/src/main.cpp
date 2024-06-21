8b60a99cea066fa3bce7a119aac1fa7d7cc88938
Justin <jtpank34@gmail.com>
2024-06-21 14:37:32 -0700

    updateS

--- c++/slam/src/main.cpp
+++ c++/slam/src/main.cpp
@@ -4,21 +4,62 @@
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include <opencv2/opencv.hpp>   // Include OpenCV API
 
+
+bool check_imu_is_supported()
+{
+    bool found_gyro = false;
+    bool found_accel = false;
+    rs2::context ctx;
+    for (auto dev : ctx.query_devices())
+    {
+        // The same device should support gyro and accel
+        found_gyro = false;
+        found_accel = false;
+        for (auto sensor : dev.query_sensors())
+        {
+            for (auto profile : sensor.get_stream_profiles())
+            {
+                if (profile.stream_type() == RS2_STREAM_GYRO)
+                    found_gyro = true;
+
+                if (profile.stream_type() == RS2_STREAM_ACCEL)
+                    found_accel = true;
+            }
+        }
+        if (found_gyro && found_accel)
+            break;
+    }
+    return found_gyro && found_accel;
+}
+
 int main(int argc, char * argv[]) try
 {
+    // Before running the example, check that a device supporting IMU is connected
+    if (!check_imu_is_supported())
+    {
+        std::cerr << "Device supporting IMU not found";
+        return EXIT_FAILURE;
+    }
+
     // Declare depth colorizer for pretty visualization of depth data
     rs2::colorizer color_map;
 
     // Declare RealSense pipeline, encapsulating the actual device and sensors
     rs2::pipeline pipe;
+
+    // Create a configuration for configuring the pipeline with a non default profile
+    rs2::config cfg;=
+    // Add streams of gyro and accelerometer to configuration
+    cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F);
+    cfg.enable_stream(RS2_STREAM_GYRO, RS2_FORMAT_MOTION_XYZ32F);
     // Start streaming with default recommended configuration
     pipe.start();
 
-    using namespace cv;
+    
     const auto window_name = "Display Image";
-    namedWindow(window_name, WINDOW_AUTOSIZE);
+    cv::namedWindow(window_name, WINDOW_AUTOSIZE);
 
-    while (waitKey(1) < 0 && getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0)
+    while (cv::waitKey(1) < 0 && cv::getWindowProperty(window_name, WND_PROP_AUTOSIZE) >= 0)
     {
         rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
         rs2::frame depth = data.get_depth_frame().apply_filter(color_map);
@@ -28,10 +69,10 @@ int main(int argc, char * argv[]) try
         const int h = depth.as<rs2::video_frame>().get_height();
 
         // Create OpenCV matrix of size (w,h) from the colorized depth data
-        Mat image(Size(w, h), CV_8UC3, (void*)depth.get_data(), Mat::AUTO_STEP);
+        cv::Mat image(cv::Size(w, h), CV_8UC3, (void*)depth.get_data(), cv::Mat::AUTO_STEP);
 
         // Update the window with new data
-        imshow(window_name, image);
+        cv::imshow(window_name, image);
     }
 
     return EXIT_SUCCESS;
