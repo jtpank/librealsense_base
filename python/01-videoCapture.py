769a6700b343d90a33c183f8fec45f44e583dd41
Justin <jtpank34@gmail.com>
2024-06-21 15:05:58 -0700

    update cause motion not work

--- python/01-videoCapture.py
+++ python/01-videoCapture.py
@@ -29,8 +29,8 @@ def convert16BitDepthToTwo8Bit3Channel(frame):
 if __name__ == "__main__":
     pipeline = rs.pipeline()
     config = rs.config()
-    config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 30)
-    config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 30)
+    # config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 30)
+    # config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 30)
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
     profile = pipeline.start(config)
