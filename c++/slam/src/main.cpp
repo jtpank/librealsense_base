02d50112c1900595f4d66f9b601353c4d71293d9
Justin <jtpank34@gmail.com>
2024-06-21 17:20:19 -0700

    mod

--- c++/slam/src/main.cpp
+++ c++/slam/src/main.cpp
@@ -61,18 +61,49 @@ int main(int argc, char * argv[]) try
 
     while (cv::waitKey(1) < 0 && cv::getWindowProperty(window_name, cv::WND_PROP_AUTOSIZE) >= 0)
     {
-        rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
-        rs2::frame depth = data.get_depth_frame().apply_filter(color_map);
+        // rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
+        // rs2::frame depth = data.get_depth_frame().apply_filter(color_map);
 
-        // Query frame size (width and height)
-        const int w = depth.as<rs2::video_frame>().get_width();
-        const int h = depth.as<rs2::video_frame>().get_height();
+        // // Query frame size (width and height)
+        // const int w = depth.as<rs2::video_frame>().get_width();
+        // const int h = depth.as<rs2::video_frame>().get_height();
 
-        // Create OpenCV matrix of size (w,h) from the colorized depth data
-        cv::Mat image(cv::Size(w, h), CV_8UC3, (void*)depth.get_data(), cv::Mat::AUTO_STEP);
+        // // Create OpenCV matrix of size (w,h) from the colorized depth data
+        // cv::Mat image(cv::Size(w, h), CV_8UC3, (void*)depth.get_data(), cv::Mat::AUTO_STEP);
 
-        // Update the window with new data
-        cv::imshow(window_name, image);
+        // // Update the window with new data
+        // cv::imshow(window_name, image);
+        // Wait for the next set of frames
+        rs2::frameset frames = pipe.wait_for_frames();
+        rs2::frameset aligned_frames = align_to.process(frames);
+        rs2::frame color_frame = aligned_frames.get_color_frame();
+        rs2::depth_frame aligned_depth_frame = aligned_frames.get_depth_frame();
+
+        // Check if both frames are valid
+        if (!color_frame || !aligned_depth_frame) {
+            continue;
+        }
+
+        // Convert RealSense frames to OpenCV matrices
+        cv::Mat color_image(cv::Size(640, 480), CV_8UC3, (void*)color_frame.get_data(), cv::Mat::AUTO_STEP);
+        cv::Mat depth_image(cv::Size(640, 480), CV_16UC1, (void*)aligned_depth_frame.get_data(), cv::Mat::AUTO_STEP);
+
+        // Apply a colormap to the depth image
+        cv::Mat depth_colormap;
+        depth_image.convertTo(depth_colormap, CV_8UC1, 0.03);
+        cv::applyColorMap(depth_colormap, depth_colormap, cv::COLORMAP_JET);
+
+        // Wrap good features (assuming fpr.wrap_goodFeats is a custom function)
+        cv::Mat colorFrame, depthFrame;
+        fpr.wrap_goodFeats(color_image, depth_colormap, depth_image, colorFrame, depthFrame);
+
+        // Concatenate color and depth frames horizontally
+        cv::Mat both_images;
+        cv::hconcat(colorFrame, depthFrame, both_images);
+
+        // Display the concatenated image
+        cv::namedWindow("test show vid", cv::WINDOW_AUTOSIZE);
+        cv::imshow("test show vid", both_images);
     }
 
     return EXIT_SUCCESS;
