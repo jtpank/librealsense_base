016726644700cba6ec13ef2dacee3b4e38849b0e
justin <jtpank34@gmail.com>
2024-06-29 08:59:54 -0700

    update

--- c++/slam/src/ir.cpp
+++ c++/slam/src/ir.cpp
@@ -1,6 +1,6 @@
 // include OpenCV header file
 #include <opencv2/opencv.hpp>
-
+#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 using namespace std;
 using namespace cv;
 
@@ -13,8 +13,10 @@ int main()
     rs2::config cfg;
 
     //Add desired streams to configuration
-    cfg.enable_stream(RS2_STREAM_INFRARED, 640, 480, RS2_FORMAT_Y8, 30);
-    cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
+    cfg.enable_stream(RS2_STREAM_INFRARED, 1, 640, 480, RS2_FORMAT_Y8, 30);
+    cfg.enable_stream(RS2_STREAM_INFRARED, 2, 640, 480, RS2_FORMAT_Y8, 30);
+    //cfg.enable_stream(RS2_STREAM_INFRARED, 640, 480, RS2_FORMAT_Y8, 30);
+    //cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
 
     //Instruct pipeline to start streaming with the requested configuration
     pipe.start(cfg);
@@ -25,11 +27,17 @@ int main()
     {
         // Camera warmup - dropping several first frames to let auto-exposure stabilize
         rs2::frameset frames;
-
+        try {
+            frames = pipe.wait_for_frames();
+        } catch (const rs2::error & e) {
+            std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n" << e.what() << std::endl;
+            continue;
+        }
         //Get each frame
-        rs2::frame ir_frame = frames.first(RS2_STREAM_INFRARED);
-        rs2::frame depth_frame = frames.get_depth_frame();
-        if (!ir_frame || !depth_frame) 
+        rs2::frame ir_frame = frames.get_infrared_frame(1);
+        rs2::frame ir_frame_2 = frames.get_infrared_frame(2);
+        //rs2::frame depth_frame = frames.get_depth_frame();
+        if (!ir_frame) 
         {
             continue;
         }
