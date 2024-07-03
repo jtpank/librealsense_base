8b3a156007189a62f001278259af1eb4e0b265f8
Justin <jtpank34@gmail.com>
2024-07-02 19:00:24 -0700

    testing

--- c++/slam/src/main.cpp
+++ c++/slam/src/main.cpp
@@ -30,13 +30,15 @@ int main()
     {
         // Declare the RealSense pipeline, encapsulating the actual device and sensors
         rs2::pipeline pipe;
-
+        rs2::colorizer color_map;
+        rs2::align align_to(RS2_STREAM_COLOR);
         //Create a configuration for configuring the pipeline with a non default profile
         rs2::config cfg;
 
         //Add desired streams to configuration
         cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F);
         cfg.enable_stream(RS2_STREAM_GYRO,  RS2_FORMAT_MOTION_XYZ32F);
+        cfg.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_BGR8, 30);
         cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
 
         //Instruct pipeline to start streaming with the requested configuration
@@ -56,23 +58,31 @@ int main()
                 std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n" << e.what() << std::endl;
                 continue;
             }
+            rs2::frameset aligned_frames = align_to.process(frames);
 
-            printf("frames length: %li\n", frames.size());
-            //Get each frame
-            // rs2::frame ir_frame = frames.get_infrared_frame(1);
-            // rs2::frame ir_frame_2 = frames.get_infrared_frame(2);
-            rs2::frame depth_frame = frames.get_depth_frame();
-            if (!depth_frame) 
+            //Get the frames
+            rs2::frame color_frame = aligned_frames.get_color_frame();
+            rs2::depth_frame aligned_depth_frame = aligned_frames.get_depth_frame();
+
+            printf("frames length: %li, aligned_frames length: %li\n", frames.size(), aligned_frames.size());
+
+            if (!aligned_depth_frame  || !color_fram) 
             {
                 continue;
             }
-            // Creating OpenCV matrix from IR image
-            cv::Mat depth_img(cv::Size(640, 480), CV_8UC1, (void*)depth_frame.get_data(), cv::Mat::AUTO_STEP);
+            // Creating OpenCV matrix for image
+            cv::Mat color_image(cv::Size(640, 480), CV_8UC3, (void*)color_frame.get_data(), cv::Mat::AUTO_STEP);
+            cv::Mat depth_image(cv::Size(640, 480), CV_16UC1, (void*)aligned_depth_frame.get_data(), cv::Mat::AUTO_STEP);
 
-            // Apply Histogram Equalization
-            // equalizeHist( ir, ir );
-            cv::applyColorMap(depth_img, depth_img, cv::COLORMAP_JET);
-            cv::imshow(window_name, depth_img);
+            cv::Mat depth_colormap;
+            depth_image.convertTo(depth_colormap, CV_8UC1, 0.03);
+            cv::applyColorMap(depth_colormap, depth_colormap, cv::COLORMAP_JET);
+
+            // Concatenate color and depth frames horizontally
+            cv::Mat both_images;
+            cv::hconcat(color_image, depth_colormap, both_images);
+
+            cv::imshow(window_name, both_images);
         }
     }
     return 0;
