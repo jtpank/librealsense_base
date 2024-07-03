bb8eecc8963b86226c264620d9b52779b2dbee1d
Justin <jtpank34@gmail.com>
2024-07-02 18:54:04 -0700

    testing

--- c++/slam/src/main.cpp
+++ c++/slam/src/main.cpp
@@ -5,8 +5,16 @@
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
 #include "FrameProcessor.hpp"
 #include <memory>
+#include <cstdio>
 using namespace std;
-using namespace cv;
+
+
+
+//TODO:
+//1. Grab depth frame, vis frame, gyro, and accel frame
+//2. Process these frames
+//3.
+
 
 
 int main()
@@ -14,26 +22,27 @@ int main()
     //Testing fp_ptr
     std::unique_ptr<FrameProcessor> fp_ptr = std::make_unique<FrameProcessor>();
 
-    fp_ptr->test_wrapGoodFeatures();
+    // fp_ptr->test_wrapGoodFeatures();
 
 
-    bool doPipeline = false;
+    bool doPipeline = true;
     if(doPipeline)
     {
-        //Contruct a pipeline which abstracts the device
+        // Declare the RealSense pipeline, encapsulating the actual device and sensors
         rs2::pipeline pipe;
 
         //Create a configuration for configuring the pipeline with a non default profile
         rs2::config cfg;
 
         //Add desired streams to configuration
-        cfg.enable_stream(RS2_STREAM_INFRARED, 1, 640, 480, RS2_FORMAT_Y8, 30);
-        cfg.enable_stream(RS2_STREAM_INFRARED, 2, 640, 480, RS2_FORMAT_Y8, 30);
-        //cfg.enable_stream(RS2_STREAM_INFRARED, 640, 480, RS2_FORMAT_Y8, 30);
-        //cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
+        cfg.enable_stream(RS2_STREAM_ACCEL, RS2_FORMAT_MOTION_XYZ32F);
+        cfg.enable_stream(RS2_STREAM_GYRO,  RS2_FORMAT_MOTION_XYZ32F);
+        cfg.enable_stream(RS2_STREAM_DEPTH, 640, 480, RS2_FORMAT_Z16, 30);
 
         //Instruct pipeline to start streaming with the requested configuration
         pipe.start(cfg);
+
+        //Display time
         const auto window_name = "Display Image";
         cv::namedWindow(window_name, cv::WINDOW_AUTOSIZE);
 
@@ -47,21 +56,23 @@ int main()
                 std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n" << e.what() << std::endl;
                 continue;
             }
+
+            printf("frames length: %i\n", frames.size());
             //Get each frame
-            rs2::frame ir_frame = frames.get_infrared_frame(1);
-            rs2::frame ir_frame_2 = frames.get_infrared_frame(2);
-            //rs2::frame depth_frame = frames.get_depth_frame();
-            if (!ir_frame) 
+            // rs2::frame ir_frame = frames.get_infrared_frame(1);
+            // rs2::frame ir_frame_2 = frames.get_infrared_frame(2);
+            rs2::frame depth_frame = frames.get_depth_frame();
+            if (!depth_frame) 
             {
                 continue;
             }
             // Creating OpenCV matrix from IR image
-            Mat ir(Size(640, 480), CV_8UC1, (void*)ir_frame.get_data(), Mat::AUTO_STEP);
+            cv::Mat ir(Size(640, 480), cv::CV_8UC1, (void*)depth_frame.get_data(), cv::Mat::AUTO_STEP);
 
             // Apply Histogram Equalization
-            equalizeHist( ir, ir );
-            applyColorMap(ir, ir, COLORMAP_JET);
-            imshow(window_name, ir);
+            // equalizeHist( ir, ir );
+            cv::applyColorMap(depth_frame, depth_frame, cv::COLORMAP_JET);
+            cv::imshow(window_name, depth_frame);
         }
     }
     return 0;
